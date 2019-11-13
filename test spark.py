19fa3236f5a0686cedc1b88161c65b3ec1b63c1e
RichardGastard <54903913+RichardGastard@users.noreply.github.com>
2019-11-13 14:18:31 +0100

    spam sms project

---
+++ test spark.py
@@ -0,0 +1,5 @@
+spark
+spark.conf.set(
+  "fs.azure.account.key.storagestudent.blob.core.windows.net",
+  "8ykAjWw0X+FVY8PnmVhnY5XyDdbpBLceLsFTbuTttdn2W76+sdAsSwYNQ9E+H8Oxlsay8/uCm8ayyWq73kB82Q=="
+)
